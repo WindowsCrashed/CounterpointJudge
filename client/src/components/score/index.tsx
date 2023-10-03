41628a4f776046b70f129519593752d240d4c0bb
WindowsCrashed <windowscrashed0000@gmail.com>
2023-10-03 15:53:18 -0300

    add countup effect

--- client/src/components/score/index.tsx
+++ client/src/components/score/index.tsx
@@ -1,15 +1,37 @@
-import { FC } from 'react'
+import { FC, useState } from 'react'
 import './style.css'
+import CountUp from 'react-countup'
 
 type ScoreProps = {
 	score: number
 }
 
 const Score: FC<ScoreProps> = ({ score }) => {
+	const [color, setColor] = useState<string>('')
+
+	const setScoreColor = () => {
+		if (score < 40) {
+			setColor('red')
+		} else if (score < 80) {
+			setColor('yellow')
+		} else {
+			setColor('green')
+		}
+	}
+
 	return (
 		<div className='score'>
 			<div className='label'>Score:</div>
-			<div className='points'>{score.toFixed(1)}</div>
+			<div className={`points ${color}`}>
+				<CountUp
+					start={0}
+					end={score}
+					duration={2.5}
+					delay={0}
+					decimals={1}
+					onStart={setScoreColor}
+				/>
+			</div>
 		</div>
 	)
 }
